50909ed4c08523901cc1e09528b85dacf2b16283
Matteo Bettini <matbet@meta.com>
2023-09-18 15:56:27 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- torchrl_paper/vmas_run.py
+++ torchrl_paper/vmas_run.py
@@ -7,7 +7,7 @@ from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base=None, config_path="../../benchmarl/conf", config_name="config")
+@hydra.main(version_base=None, config_path="../benchmarl/conf", config_name="config")
 def hydra_experiment(cfg: DictConfig) -> None:
     hydra_choices = HydraConfig.get().runtime.choices
     task_name = hydra_choices.task
