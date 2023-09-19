63f927c5b97930528dc4463a9e03da90758ad467
Matteo Bettini <matbet@meta.com>
2023-09-19 10:51:04 +0100

    [Config] New default config and test config
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- test/test_algorithms.py
+++ test/test_algorithms.py
@@ -1,18 +1,22 @@
-import pathlib
+import importlib
 
 import pytest
-
 from benchmarl.algorithms import algorithm_config_registry
+
 from benchmarl.environments import VmasTask
-from benchmarl.experiment import Experiment, ExperimentConfig
+from benchmarl.experiment import Experiment
 from benchmarl.models.common import SequenceModelConfig
 from benchmarl.models.mlp import MlpConfig
 from torch import nn
 
 
+_has_vmas = importlib.util.find_spec("vmas") is not None
+
+
+@pytest.mark.skipif(not _has_vmas, reason="VMAS not found")
 @pytest.mark.parametrize("algo_config", algorithm_config_registry.values())
 @pytest.mark.parametrize("continuous", [True, False])
-def test_all_algos_balance(algo_config, continuous):
+def test_all_algos_vmas(algo_config, continuous, experiment_config):
     task = VmasTask.BALANCE.get_from_yaml()
     model_config = SequenceModelConfig(
         model_configs=[
@@ -20,14 +24,6 @@ def test_all_algos_balance(algo_config, continuous):
             MlpConfig(num_cells=[4], activation_class=nn.Tanh, layer_class=nn.Linear),
         ],
         intermediate_sizes=[5],
-    )
-    experiment_config: ExperimentConfig = ExperimentConfig.get_from_yaml(
-        str(
-            pathlib.Path(__file__).parent
-            / "conf"
-            / "experiment"
-            / "base_experiment.yaml"
-        )
     )
     experiment_config.prefer_continuous_actions = continuous
 
