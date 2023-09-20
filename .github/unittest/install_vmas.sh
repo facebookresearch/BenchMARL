f7e99c091406c86c9f8ebc0069a24d12ed540216
Matteo Bettini <matbet@meta.com>
2023-09-20 22:10:21 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- .github/unittest/install_vmas.sh
+++ .github/unittest/install_vmas.sh
@@ -1,2 +1,7 @@
 
 python -m pip install vmas
+apt-get install -y python-opengl
+apt-get install -y xvbf
+apt-get install -y x11-utils
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
