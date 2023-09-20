f8c62ca5ce9476b2130835b08b6d943dd825db02
Matteo Bettini <matbet@meta.com>
2023-09-20 22:56:29 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- .github/unittest/install_vmas.sh
+++ .github/unittest/install_vmas.sh
@@ -1,11 +1,6 @@
 
 python -m pip install vmas
 sudo apt-get update
-sudo apt-get install --fix-missing libgl1-mesa-dev
-sudo apt-get install -y python-opengl
-sudo apt install -y freeglut3-devel
-sudo apt-get install -y libglx-dev
-sudo apt-get install -y xvbf
-sudo apt-get install -y x11-utils
+sudo apt-get install python3-opengl xvfb
 export DISPLAY=':99.0'
 Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
