55841b0a34ffc42e45d8be0cd79c97f23e1ab155
Matteo Bettini <matbet@meta.com>
2023-09-20 22:36:13 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- .github/unittest/install_vmas.sh
+++ .github/unittest/install_vmas.sh
@@ -1,5 +1,7 @@
 
 python -m pip install vmas
+sudo apt-get update
+sudo apt-get install --fix-missing libgl1-mesa-dev
 sudo apt-get install -y python-opengl
 sudo apt-get install -y xvbf
 sudo apt-get install -y x11-utils
