6655a129cae62b7ee0cf311937aa5f53ce7c8569
Matteo Bettini <matbet@meta.com>
2023-09-20 21:58:36 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author="Matteo Bettini",
     author_email="mb2389@cl.cam.ac.uk",
     packages=find_packages(),
-    install_requires=["torchrl"],
+    install_requires=["torchrl", "tqdm", "hydra-core"],
     include_package_data=True,
 )
