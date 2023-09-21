a94d9599abd54ce62b1a190db77653be0b4a9fcd
Matteo Bettini <matbet@meta.com>
2023-09-21 10:28:17 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- test/test_vmas.py
+++ test/test_vmas.py
@@ -7,6 +7,8 @@ from benchmarl.algorithms.common import AlgorithmConfig
 from benchmarl.environments import Task, VmasTask
 from benchmarl.experiment import Experiment
 
+from utils_experiment import ExperimentUtils
+
 _has_vmas = importlib.util.find_spec("vmas") is not None
 
 
@@ -24,9 +26,7 @@ class TestVmas:
         mlp_sequence_config,
     ):
         task = task.get_from_yaml()
-
         experiment_config.prefer_continuous_actions = continuous
-
         experiment = Experiment(
             algorithm_config=algo_config.get_from_yaml(),
             model_config=mlp_sequence_config,
@@ -45,38 +45,9 @@ class TestVmas:
         experiment_config,
         mlp_sequence_config,
     ):
-
-        task = task.get_from_yaml()
-        experiment = Experiment(
-            algorithm_config=algo_config.get_from_yaml(),
+        ExperimentUtils.check_experiment_loading(
+            algo_config=algo_config.get_from_yaml(),
             model_config=mlp_sequence_config,
-            seed=0,
-            config=experiment_config,
-            task=task,
+            experiment_config=experiment_config,
+            task=task.get_from_yaml(),
         )
-        experiment.run()
-        policy = experiment.policy
-        losses = experiment.losses
-        exp_folder = experiment.folder_name
-        experiment_config.n_iters = 6
-        experiment_config.restore_file = exp_folder / "checkpoints" / "checkpoint_3.pt"
-        experiment = Experiment(
-            algorithm_config=algo_config.get_from_yaml(),
-            model_config=mlp_sequence_config,
-            seed=0,
-            config=experiment_config,
-            task=task,
-        )
-        for param1, param2 in zip(
-            list(experiment.policy.parameters()), list(policy.parameters())
-        ):
-            assert (param1 == param2).all()
-        for loss1, loss2 in zip(experiment.losses.values(), losses.values()):
-            for param1, param2 in zip(
-                list(loss1.parameters()), list(loss2.parameters())
-            ):
-                assert (param1 == param2).all()
-        assert experiment.n_iters_performed == 3
-        assert experiment.folder_name == exp_folder
-        experiment.run()
-        assert experiment.n_iters_performed == 6
