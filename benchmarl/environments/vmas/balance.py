a4a76cac822ff4266b3e1ecd36c70b4852d62e9a
Matteo Bettini <matbet@meta.com>
2023-10-05 17:45:16 +0100

    update current vmas tasks
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- benchmarl/environments/vmas/balance.py
+++ benchmarl/environments/vmas/balance.py
@@ -5,3 +5,5 @@ from dataclasses import dataclass, MISSING
 class TaskConfig:
     max_steps: int = MISSING
     n_agents: int = MISSING
+    random_package_pos_on_line: bool = MISSING
+    package_mass: float = MISSING
