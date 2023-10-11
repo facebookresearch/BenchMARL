7d19bfbe6698a58ef03dd34a58ab8538116ea397
Matteo Bettini <matbet@meta.com>
2023-10-11 10:49:35 +0100

    copyright
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- benchmarl/benchmark.py
+++ benchmarl/benchmark.py
@@ -1,3 +1,9 @@
+#  Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+#  This source code is licensed under the license found in the
+#  LICENSE file in the root directory of this source tree.
+#
+
 from typing import Iterator, Optional, Sequence, Set
 
 from benchmarl.algorithms.common import AlgorithmConfig
