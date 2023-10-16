2c6a5b1df30f8dd2bb624eeac4e65297545a2938
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-10-16 15:22:25 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="benchmarl",
-    version="0.0.1",
+    version="0.0.2",
     description="BenchMARL",
     url="https://github.com/facebookresearch/BenchMARL",
     author="Matteo Bettini",
