dd880a4e77d3d329ad37632e196a64ee47ad7753
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-11-22 18:16:26 +0000

    Amend

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 
 html_theme = "benchmarl_sphinx_theme"
