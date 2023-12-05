db6696dc65482e6e674e5daa87890c23e53d9230
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-12-05 08:46:16 +0000

    [BugFix] Remove logger warning

--- benchmarl/experiment/logger.py
+++ benchmarl/experiment/logger.py
@@ -5,6 +5,7 @@
 #
 
 import json
+import os
 from pathlib import Path
 from typing import Dict, List, Optional
 
@@ -214,9 +215,12 @@ class Logger:
                 evaluation_step=total_frames
                 // self.experiment_config.evaluation_interval,
             )
+            json_file = str(self.json_writer.path)
             for logger in self.loggers:
                 if isinstance(logger, WandbLogger):
-                    logger.experiment.save(str(self.json_writer.path))
+                    logger.experiment.save(
+                        json_file, base_path=os.path.dirname(json_file)
+                    )
 
         self.log(to_log, step=step)
         if video_frames is not None:
