bccf368a449b54d2dde71d63c7ace58e63a41fab
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-02-23 11:19:31 +0000

    ci

--- .github/unittest/install_dependencies.sh
+++ .github/unittest/install_dependencies.sh
@@ -1,11 +1,9 @@
 
 
 python -m pip install --upgrade pip
-python -m pip install flake8 pytest pytest-cov hydra-core tqdm torch_geometric
+python -m pip install flake8 pytest pytest-cov hydra-core tqdm torch torch_geometric
 
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
-
-python -m pip install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cpu --force-reinstall
 
 pip install torchrl
 
