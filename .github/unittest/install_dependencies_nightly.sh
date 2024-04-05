35f931447edfe834213312f77e9b832e5772e2ec
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-04-05 19:58:59 +0200

    amend

--- .github/unittest/install_dependencies_nightly.sh
+++ .github/unittest/install_dependencies_nightly.sh
@@ -11,7 +11,7 @@ python -m pip install torch
 
 cd ../BenchMARL
 pip install -e .
-pip uninstall -y torchrl tensordict
+pip uninstall --yes torchrl tensordict
 
 cd ..
 python -m pip install git+https://github.com/pytorch-labs/tensordict.git
