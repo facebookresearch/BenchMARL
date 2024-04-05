6a06084f545ab11612c7e97f4bd0b01fb861823e
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-04-05 19:20:37 +0200

    amend

--- .github/unittest/install_dependencies_nightly.sh
+++ .github/unittest/install_dependencies_nightly.sh
@@ -9,10 +9,12 @@ python -m pip install torch
 # Not using nightly torch
 # python -m pip install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cpu --force-reinstall
 
+cd ../BenchMARL
+pip install -e .
+pip uninstal torchrl tensordict
+
 cd ..
 python -m pip install git+https://github.com/pytorch-labs/tensordict.git
 git clone https://github.com/pytorch/rl.git
 cd rl
 python setup.py develop
-cd ../BenchMARL
-pip install -e .
