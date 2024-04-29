c5999c6f68b442494d5f71bfefe343dac243f447
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-04-29 15:59:40 +0100

    amend

--- benchmarl/algorithms/qmix.py
+++ benchmarl/algorithms/qmix.py
@@ -177,13 +177,19 @@ class Qmix(Algorithm):
 
     def get_mixer(self, group: str) -> TensorDictModule:
         n_agents = len(self.group_map[group])
-        group_observation_key = list(self.observation_spec[group].keys())[0]
 
         if self.state_spec is not None:
-            global_state_key = list(self.state_spec.keys())[0]
+            global_state_key = list(self.state_spec.keys(True, True))[0]
             state_shape = self.state_spec[global_state_key].shape
             in_keys = [(group, "chosen_action_value"), global_state_key]
         else:
+            group_observation_keys = list(self.observation_spec[group].keys(True, True))
+            if len(group_observation_keys) >= 1:
+                raise ValueError(
+                    "QMIX called without a global state and multiple observation keys, currently the mixer"
+                    "takes only one observation key, please raise an issue if you need this fauture."
+                )
+            group_observation_key = group_observation_keys[0]
             state_shape = self.observation_spec[group, group_observation_key].shape
             in_keys = [(group, "chosen_action_value"), (group, group_observation_key)]
 
