2a337e8ab93cf157ce1dc055bb4cd1f912ea00b2
Matteo Bettini <55539777+matteobettini@users.noreply.github.com>
2024-06-17 12:13:56 +0200

    [Docs] Warn latest is unstable (#101)
    * docs
    
    * docs
    
    * docs

---
+++ docs/source/_static/js/version_alert.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+function warnOnLatestVersion() {
+  if (!window.READTHEDOCS_DATA || window.READTHEDOCS_DATA.version !== "latest") {
+    return;  // not on ReadTheDocs and not latest.
+  }
+
+  var note = document.createElement('div');
+  note.setAttribute('class', 'admonition note');
+  note.innerHTML = "<p class='first admonition-title'>Note</p> " +
+    "<p> " +
+    "This documentation is for an <b>unreleased development version</b>. " +
+    "Click <a href='/en/stable'><b>here</b></a> to access the documentation of the current stable release." +
+    "</p>";
+
+  var parent = document.querySelector('#benchmarl');
+  if (parent)
+    parent.insertBefore(note, parent.querySelector('h1'));
+}
+
+document.addEventListener('DOMContentLoaded', warnOnLatestVersion);
