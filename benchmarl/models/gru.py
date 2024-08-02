a7d0627b3835013f35850ec1decb5b45188d97aa
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-08-02 10:14:51 +0200

    copyright

--- benchmarl/models/gru.py
+++ benchmarl/models/gru.py
@@ -4,12 +4,6 @@
 #  LICENSE file in the root directory of this source tree.
 #
 
-#  Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-#  This source code is licensed under the license found in the
-#  LICENSE file in the root directory of this source tree.
-#
-
 from __future__ import annotations
 
 from dataclasses import dataclass, MISSING
