91672d2406686d7ad956f00dd960325749e90d8e
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-09-09 20:57:12 +0200

    add examples

--- examples/checkpointing/resume_experiment.sh
+++ examples/checkpointing/resume_experiment.sh
@@ -7,4 +7,4 @@
 #
 
 python benchmarl/run.py task=vmas/balance algorithm=mappo experiment.max_n_iters=3 experiment.on_policy_collected_frames_per_batch=100 experiment.checkpoint_interval=100
-python benchmarl/run.py task=vmas/balance algorithm=mappo experiment.max_n_iters=6 experiment.on_policy_collected_frames_per_batch=100 experiment.restore_file="/hydra_experiment_folder/checkpoint/checkpoint_300.pt"
+python benchmarl/resume.py /hydra_experiment_folder/checkpoint/checkpoint_200.pt
