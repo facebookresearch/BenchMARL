c3d88b459696dd7459d2107d83ddd879fa45dbca
Matteo Bettini <mb2389@cl.cam.ac.uk>
2025-03-20 17:46:27 +0000

    minor seeding and logging

--- .github/unittest/install_magent2.sh
+++ .github/unittest/install_magent2.sh
@@ -1,7 +1,6 @@
 
 
 pip install git+https://github.com/Farama-Foundation/MAgent2
-pip install "pettingzoo[all]==1.24.3"
 
 sudo apt-get update
 sudo apt-get install python3-opengl xvfb
--- .github/unittest/install_pettingzoo.sh
+++ .github/unittest/install_pettingzoo.sh
@@ -1,4 +1,4 @@
 
-pip install "pettingzoo[all]==1.24.3"
+pip install "pettingzoo[mpe,sisl]==1.24.3"
 sudo apt-get update
 sudo apt-get install python3-opengl xvfb
