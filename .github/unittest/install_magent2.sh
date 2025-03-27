44bb7b3270cd96f6cd308cc1275b75923e8050cc
Matteo Bettini <mb2389@cl.cam.ac.uk>
2025-03-27 12:12:53 +0000

    amend

--- .github/unittest/install_magent2.sh
+++ .github/unittest/install_magent2.sh
@@ -1,7 +1,7 @@
 
 
-# pip install git+https://github.com/Farama-Foundation/MAgent2 temporary
-pip install git+https://github.com/Farama-Foundation/MAgent2@764b0d4cf32e7e2eb1d0bc05b7984daaf43b5989
+# pip install git+https://github.com/Farama-Foundation/MAgent2 temporary TODO wait for fix
+pip install git+https://github.com/Farama-Foundation/MAgent2@9706565fab98ae3dc90dc13b40569a392bf589aa
 
 sudo apt-get update
 sudo apt-get install python3-opengl xvfb
