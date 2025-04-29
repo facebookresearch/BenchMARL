1af20eca34838175db880851074449cbc4363b26
Matteo Bettini <mb2389@cl.cam.ac.uk>
2025-04-29 17:31:46 +0200

    amend

--- .github/unittest/install_dependencies.sh
+++ .github/unittest/install_dependencies.sh
@@ -1,8 +1,9 @@
 
 
 python -m pip install --upgrade pip
-python -m pip install flake8 pytest pytest-cov hydra-core tqdm torchrl torchvision "av<14"
-python -m pip install torch_geometric
+python -m pip install flake8 pytest pytest-cov hydra-core tqdm
+python -m pip install torchrl
+python -m pip install torch_geometric torchvision "av<14"
 
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 
