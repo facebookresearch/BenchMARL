9ed99a79c9d7fccd9209aba00bdcd2668b3a772f
Vincent Moens <vmoens@meta.com>
2025-05-01 13:33:39 +0100

    [Release] 1.5.0 (#199)
    * Install cmake and pybind11 for tensordict
    
    * Update install_dependencies_nightly.sh
    
    * Update install_dependencies_nightly.sh
    
    * amend
    
    * deprecations
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * empty
    
    * empty
    
    * amend
    
    ---------
    
    Co-authored-by: Matteo Bettini <55539777+matteobettini@users.noreply.github.com>
    Co-authored-by: Matteo Bettini <mb2389@cl.cam.ac.uk>

--- .github/unittest/install_dependencies.sh
+++ .github/unittest/install_dependencies.sh
@@ -1,11 +1,11 @@
 
 
 python -m pip install --upgrade pip
-python -m pip install flake8 pytest pytest-cov hydra-core tqdm torch torch_geometric torchvision "av<14"
+python -m pip install flake8 pytest pytest-cov hydra-core tqdm
+python -m pip install torchrl
+python -m pip install torch_geometric torchvision "av<14"
 
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
-
-pip install torchrl
 
 cd ../BenchMARL
 pip install -e .
