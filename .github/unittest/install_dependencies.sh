3319247e2f72a51d1f9453d3195f6df81b5e9bb3
Matteo Bettini <mb2389@cl.cam.ac.uk>
2025-10-01 00:22:20 +0200

    test

--- .github/unittest/install_dependencies.sh
+++ .github/unittest/install_dependencies.sh
@@ -2,14 +2,10 @@
 
 python -m pip install --upgrade pip
 python -m pip install flake8 pytest pytest-cov hydra-core tqdm
-python -m pip install torchrl
+# python -m pip install torchrl
 python -m pip install torch_geometric torchvision "av<14"
 
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 
 cd ../BenchMARL
 pip install -e .
-
-# install cmake
-conda install anaconda::cmake -y
-python -m pip install "pybind11[global]"
