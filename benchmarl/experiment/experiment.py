c6413efeb11c279e250352c2aed5f4d01a0a8c6f
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2025-10-04 16:07:00 +0200

    patch experiment folder

--- benchmarl/experiment/experiment.py
+++ benchmarl/experiment/experiment.py
@@ -1035,6 +1035,7 @@ class Experiment(CallbackNotifier):
             critic_model_config = pickle.load(f)
             callbacks = pickle.load(f)
         task.config = task_config
+        experiment_config.save_folder = experiment_folder
         experiment_config.restore_file = restore_file
         experiment = Experiment(
             task=task,
