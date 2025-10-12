b1b3663509a3b57f1a965d854e49ef94279aa6b4
Matteo Bettini <mb2389@cl.cam.ac.uk>
2025-10-13 00:20:21 +0200

    test

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     author="Matteo Bettini",
     author_email="mb2389@cl.cam.ac.uk",
     install_requires=[
-        "torchrl~=0.10.0",
+        "torchrl>=0.8,<0.11",
         "tqdm",
         "hydra-core",
         "torchvision",
