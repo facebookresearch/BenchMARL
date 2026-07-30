6e22862ed9288802f514668a63241209cf269c60
YemenLinux <bash@yemenmobile.com.ye>
2026-07-30 22:21:17 +0300

    update torchrl dependency

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
     author="Matteo Bettini",
     author_email="mb2389@cl.cam.ac.uk",
     install_requires=[
-        "torchrl>=0.10,<0.12",
+        # "torchrl>=0.10,<0.12",
+        "torchrl",
         "tqdm",
         "hydra-core",
         "torchvision",
